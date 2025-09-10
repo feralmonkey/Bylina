1942100dbb8e577b2749ca3334cee17d367318ef
Travis Golob <feralmonkey@users.noreply.github.com>
2025-09-10 07:45:58 -0700

    Added additional assets

---
+++ src/components/TextComponent.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+#include <glm/glm.hpp>
+#include <SDL.h>
+
+struct TextComponent {
+	glm::vec2 position;
+
+	// width and height in characters (each character is 8 pixels)
+	int width;
+	int height;
+
+	std::string text;
+	bool hasSelector;
+
+	TextComponent(glm::vec2 position = glm::vec2(0), std::string text = "", bool hasSelector = false) {
+		this->position = position;
+		this->text = text;
+		this->hasSelector = hasSelector;
+	}
+};
